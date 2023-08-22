ce1fd618818d372484250c7c59ebed5915d18bc1
daisy.chepkemoi <daisy.chepkemoi@cellulant.io>
2023-08-22 19:56:19 +0300

    added git ignore

--- src/test/java/org/example/context/Constants.java
+++ src/test/java/org/example/context/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
 	// location of driver
 	// etc
 
+
 	public static final String BASE_URL = getPropertiesFile().getProperty("BASE_URL");
 	public static final String DRIVER_LOCATION = getPropertiesFile().getProperty("DRIVER_PATH");
 }
